1255bb0439782ec103f5d4ad870a77f0f7d849f0
Will23N <105712914+Will23N@users.noreply.github.com>
2022-05-22 13:47:11 +0200

    Create search_book.php

---
+++ resources/views/search_book.php
@@ -0,0 +1,9 @@
+<tr>
+	<td><%= obj.book_id %></td>
+	<td><%= obj.title %></td>
+	<td><%= obj.author %></td>
+	<td><%= obj.publisher %></td>
+	<td><%= obj.category %></td>
+  <td><%= obj.barcode %></td>
+	<td><%= obj.avaliability %></td>
+</tr>
